89012cd8b5184163efd74ca42f554aca48785619
tianyamingyuexin <3247446295@qq.com>
2018-04-17 11:48:30 +0800

    Initial Commit

---
+++ RuntimeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RuntimeDemo
+//
+//  Created by guangshu01 on 2018/4/17.
+//  Copyright © 2018年 guangshu01. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
